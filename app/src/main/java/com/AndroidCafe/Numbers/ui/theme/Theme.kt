65802efc5dfc04794d47ec099d5ac45ab174b85f
vinchamp77 <vinchamp77@gmail.com>
2022-08-20 14:28:06 +1000

    Color theme

--- app/src/main/java/com/AndroidCafe/Numbers/ui/theme/Theme.kt
+++ app/src/main/java/com/AndroidCafe/Numbers/ui/theme/Theme.kt
@@ -5,18 +5,17 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color(0xFF456eab),
+    primaryVariant = Color(0xFF456eab),
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color(0xFF456eab),
+    primaryVariant = Color(0xFF456eab),
 
     /* Other default colors to override
     background = Color.White,
